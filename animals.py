2d3e6b34a26ffd5523ffdf0aaf96d79ed2d45530
Christan Grant <cgrant@ou.edu>
2022-02-08 20:46:56 +0000

    added animals.py file from class

---
+++ animals.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import json
+import logging
+import time
+
+def main():
+    print("world")
+    myint = 1231231312
+    myfloat = 0.4
+    mystring = """
+    stuff
+    stuff on a new line
+    last line"""
+
+    print(f"{myint} -- {myfloat} --{mystring}")
+    print(r"Hello\t\tHello")
+
+
+if __name__ == '__main__':
+    # something
+    print("Hello")
+    main()
