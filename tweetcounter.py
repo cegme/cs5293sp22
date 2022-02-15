b2961484ea145a9a872aeed019f55c2fed060171
Christan Grant <cgrant@ou.edu>
2022-02-15 20:45:44 +0000

    End of class 9

--- tweetcounter.py
+++ tweetcounter.py
@@ -1,10 +1,16 @@
 
+import argparse
 
-def main():
+def main(term):
 	"""Main function to start the program"""
-	pass
+	print(f"The terms: {term}")
 
 
 if __name__ == '__main__':
-	main()
-	print("Hello")
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--term", type=str, required=True, help="The term to look up in theset of tweets")
+
+	args = parser.parse_args()
+	print(args)
+	main(args.term)
